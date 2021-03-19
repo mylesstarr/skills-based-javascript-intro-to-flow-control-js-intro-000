974f124e53cb4e651ec0f5deb49d6023ed9c5f7e
Myles Starr <mylesstarr@gmail.com>
2021-03-19 18:31:08 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,17 @@
 function basicTeenager(age) {
+  if (age >= 13 && age <= 19) {
+  return "You are a teenager!"
+}}
 
+function teenager(age){
+return true
 }
-
-function teenager(age) {
-
+function ageChecker(age){
+return true
 }
-
-function ageChecker(age) {
-
+function ternaryTeenager(age){
+return true
 }
-
-function ternaryTeenager(age) {
-
-}
-
-function switchAge(age) {
-
+function switchAge(age){
+return true
 }
